ed5ba08e04cce8fcfc3e5b37ae88dbcd07935286
João Pedro Nolasco <nolascojoaops@gmail.com>
2024-04-24 15:43:50 -0300

    CH-02: Add validation rules for controllers

--- Spring in Action/chapter-02/taco-cloud/src/main/java/tacos/controllers/DesignTacoController.java
+++ Spring in Action/chapter-02/taco-cloud/src/main/java/tacos/controllers/DesignTacoController.java
@@ -6,12 +6,14 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import tacos.models.Ingredient;
 import tacos.models.Ingredient.Type;
@@ -62,7 +64,10 @@ public class DesignTacoController {
 	}
 	
 	@PostMapping
-	public String processTaco(Taco taco, @ModelAttribute TacoOrder tacoOrder) {
+	public String processTaco(@Valid Taco taco, Errors erros, @ModelAttribute TacoOrder tacoOrder) {
+		if (erros.hasErrors()) {
+			return "design";
+		}
 		tacoOrder.addTaco(taco);
 		log.info("Processing taco: {}",taco);
 		return "redirect:/orders/current";
